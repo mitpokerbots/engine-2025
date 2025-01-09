7e1db7f96f713f0fc89ccf694738ef0a7bfc3ce2
jennyjin <jinyf@mit.edu>
2025-01-08 23:16:49 -0500

    Update player.py
    Preflop

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -102,6 +102,138 @@ class Player(Bot):
         my_bounty = round_state.bounties[active]  # your current bounty rank
         my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
         opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+    
+        #Version 1 - Logic Bot
+        my_cards = round_state.hands[active]
+        card1 = my_cards[0]
+        card2 = my_cards[1]
+
+        rank1 = card1[0]
+        suit1 = card1[1]
+        rank2 = card2[0]
+        suit2 = card2[1]
+
+        card_type = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k", "A"]
+
+        big_blind = bool(active)  # True if you are the big blind
+
+        if street == 0:
+            if RaiseAction in legal_actions:
+                min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+                min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+                max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+                
+                if rank1  == "A" and rank2 == "A":
+                    return RaiseAction(min(max(min_cost, 85), max_cost))
+                if rank1  == "K" and rank2 == "K":
+                    return RaiseAction(min(max(min_cost, 82), max_cost))
+                if rank1  == "Q" and rank2 == "Q":
+                    return RaiseAction(min(max(min_cost, 80), max_cost))
+                if rank1  == "J" and rank2 == "J":
+                    return RaiseAction(min(max(min_cost, 77), max_cost))
+                if rank1  == "10" and rank2 == "10":
+                    return RaiseAction(min(max(min_cost, 75), max_cost))
+                if rank1  == "9" and rank2 == "9":
+                    return RaiseAction(min(max(min_cost, 72), max_cost))
+                    
+            if CallAction in legal_actions:
+                if rank1 == rank2:
+                    if rank1 == "4" or rank1 == "5" or rank1 == "6" or rank1 == "7" or rank1 == "8": #Pairs 4-8
+                        return CallAction()
+                    else:
+                        if opp_pip <= 75:
+                            return CallAction()
+                    
+                if rank1 == "A" or rank2 == "A": #Has a A (2-K suited, 6-K unsuited)
+                    if suit1 == suit2:  
+                        return CallAction()
+                    else:
+                        for i in range(4, len(card_type)-1):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                        if opp_pip <= 75:
+                            return CallAction()
+                
+                if rank1 == "K" or rank2 == "K": #Has a K (7-Q suited, 9-Q unsuited)
+                    if suit1 == suit2:  
+                        for i in range(5, len(card_type)-2):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                        if opp_pip <= 75:
+                            return CallAction()
+                    else:
+                        for i in range(7, len(card_type)-2):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                        if opp_pip <= 75:
+                            return CallAction()
+                            
+                if rank1 == "Q" or rank2 == "Q": #Has a Q (9-J suited, 10-J unsuited)
+                    if suit1 == suit2:  
+                        for i in range(7, len(card_type)-3):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                        if opp_pip <= 75:
+                            return CallAction()
+                    else:
+                        for i in range(8, len(card_type)-3):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                        if opp_pip <= 75:
+                            return CallAction()
+                            
+                if rank1 == "J" or rank2 == "J": #Has a J (10 suited)
+                    if suit1 == suit2 and suit1 == "10":
+                        return CallAction()
+                    if opp_pip <= 75:
+                            return CallAction()
+                
+                if rank1 == "10" or rank2 == "10": #Has a 10 (7-9 suited, 9 unsuited)
+                    if suit1 == suit2 and opp_pip <= 75:  
+                        for i in range(5, 8):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                        
+                    if suit1 != suit2 and opp_pip <= 75:
+                        if suit1 == "9":
+                            return CallAction()
+                
+                if rank1 == "9" or rank2 == "9": #Has a 9 (8-9 suited)
+                    if suit1 == suit2 and opp_pip <= 75:  
+                        for i in range(6, 8):
+                            if rank1 == card_type[i]:
+                                return CallAction()
+                            if rank2 == card_type[i]:
+                                return CallAction()
+                    
+                    
+            if CheckAction in legal_actions:
+                return CheckAction()
+            
+            if FoldAction in legal_actions:
+                return FoldAction()
+                        
+
+                        
+                        
+                    
 
                 
         if RaiseAction in legal_actions:
