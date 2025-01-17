7f55207cc2bc5cf07a9ea64da02a79792a9bda07
Joy12343 <jzhuo1919@gmail.com>
2025-01-16 20:30:48 -0500

    added PreflopHistory class

--- python_skeleton/preflop.py
+++ python_skeleton/preflop.py
@@ -25,12 +25,16 @@ bounty_rank = previous_state.bounties[active]  # your bounty rank
 # The following is a demonstration of accessing illegal information (will not work)
 opponent_bounty_rank = previous_state.bounties[1-active]  # attempting to grab opponent's bounty rank
 '''
+DISCRETE_ACTIONS = ["k", "bMIN", "bMID", "bMAX", "c", "f"]
+
 class PreflopHistory(base.History):
 
-    def __init__(self, history: List[Action] = [], sample_id=0, round_state = None):
+    def __init__(self, history: List[Action] = [], game_state = None, round_state = None, sample_id=0, terminal_state = None):
         super().__init__(history)
+        self.game_state = game_state
         self.round_state = round_state
         self.sample_id = sample_id
+        self.terminal_state = terminal_state
 
     def is_terminal(self):
         if len(self.history) == 0:
@@ -44,7 +48,92 @@ class PreflopHistory(base.History):
         if self.is_chance():
             return []
         elif not self.is_terminal():
-            legal_action = self.round_state.legal_actions()
-            return list(legal_action)
+            assert (
+                not self._game_stage_ended()
+            )  # game_stage_ended would mean that it is a chance node
+
+            if len(self.history) == 2:
+                return ["c", "bMIN", "bMID", "bMAX", "f"]
+            elif self.history[-1] == "bMIN":
+                return ["bMID", "bMAX", "f", "c"]
+            elif self.history[-1] == "bMID":
+                return ["bMAX", "f", "c"]
+            elif self.history[-1] == "bMAX":
+                return ["f", "c"]
+            else:
+                return ["k", "bMIN", "bMID", "bMAX"]
         else:
             raise Exception("Cannot call actions on a terminal history")
+
+    def player(self):
+        """
+        1. ['AkTh', 'QdKd', 'bMID', 'c', '/', 'Qh2d3s4h5s']
+        """
+        if len(self.history) < 2:
+            return -1
+        elif self._game_stage_ended():
+            return -1
+        elif self.history[-1] == "/":
+            return -1
+        else:
+            return (len(self.history) + 1) % 2
+
+    def _game_stage_ended(self):
+        return (
+            (self.history[-1] == "c" and len(self.history) > 3)
+            or self.history[-1] == "f"
+            or self.history[-2:] == ["c", "k"]
+        )
+
+    def is_chance(self):
+        return super().is_chance()
+
+    def sample_chance_outcome(self):
+        assert self.is_chance()
+
+        if len(self.history) == 0:
+            player_hand = self.round_state.hands[self.active]
+            return "".join(player_hand)
+        elif len(self.history) == 1:
+            opponent_hand = self.round_state.hands[1-self.active]
+            return "".join(opponent_hand)
+        elif self.history[-1] != "/":
+            return "/"
+        else:
+            board_cards = self.round_state.deck[:self.round_state.street]
+            return "".join(board_cards)
+
+    def terminal_utility(self, i: Player) -> int:
+        assert self.is_terminal()  # We can only call the utility for a terminal history
+        assert i in [0, 1]  # Only works for 2 player games for now
+
+    def _get_total_pot_size(self, round_state):
+        # Pot size is the sum of contributions from both players
+        pot_size = round_state.STARTING_STACK - round_state.stacks[0] + \
+                round_state.STARTING_STACK - round_state.stacks[1]
+
+        # Latest bet is the maximum contribution in the current round
+        latest_bet = max(round_state.pips)
+
+        return pot_size, latest_bet
+
+    def __add__(self, action: Action):
+        new_history = PreflopHistory(self.history + [action], self.sample_id)
+        return new_history
+
+    def get_infoSet_key(self) -> List[Action]:
+        """
+        This is where we abstract away cards and bet sizes.
+        """
+        assert not self.is_chance()
+        assert not self.is_terminal()
+
+        player = self.player()
+        infoset = []
+        # ------- CARD ABSTRACTION -------
+        infoset.append(str(get_preflop_cluster_id(self.history[player])))
+        for i, action in enumerate(self.history):
+            if action in DISCRETE_ACTIONS:
+                infoset.append(action)
+
+        return infoset
