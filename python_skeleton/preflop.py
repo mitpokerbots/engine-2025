b48e09f9b810f29418fe6d7445960cb890a7b8f8
Joy12343 <jzhuo1919@gmail.com>
2025-01-16 22:37:29 -0500

    updated get_infoSet_key function

--- python_skeleton/preflop.py
+++ python_skeleton/preflop.py
@@ -3,7 +3,7 @@ from skeleton.states import GameState, TerminalState, RoundState
 from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
 from skeleton.bot import Bot
 from skeleton.runner import parse_args, run_bot
-from typing import NewType
+from typing import NewType, List
 
 ''' Game Reference
 my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
@@ -28,45 +28,19 @@ opponent_bounty_rank = previous_state.bounties[1-active]  # attempting to grab o
 '''
 DISCRETE_ACTIONS = ["k", "bMIN", "bMID", "bMAX", "c", "f"]
 
-Action = New
+Action = NewType("Action", str)
 
 class PreflopHistory():
 
-    def __init__(self, history: List[Action] = [], game_state = None, round_state = None, sample_id=0, terminal_state = None):
-        super().__init__(history)
-        self.game_state = game_state
-        self.round_state = round_state
+    def __init__(self, history: List[Action] = [], sample_id = 0):
+        self.history = history
         self.sample_id = sample_id
-        self.terminal_state = terminal_state
 
     def is_terminal(self):
-        if len(self.history) == 0:
-            return False
-        if len(self.history[-1]) == 10:  # show community cards
-            return True
-        else:
-            return False
+        num_round = len(self.history)
+        street = len(self.history[-1])
 
-    def actions(self):
-        if self.is_chance():
-            return []
-        elif not self.is_terminal():
-            assert (
-                not self._game_stage_ended()
-            )  # game_stage_ended would mean that it is a chance node
-
-            if len(self.history) == 2:
-                return ["c", "bMIN", "bMID", "bMAX", "f"]
-            elif self.history[-1] == "bMIN":
-                return ["bMID", "bMAX", "f", "c"]
-            elif self.history[-1] == "bMID":
-                return ["bMAX", "f", "c"]
-            elif self.history[-1] == "bMAX":
-                return ["f", "c"]
-            else:
-                return ["k", "bMIN", "bMID", "bMAX"]
-        else:
-            raise Exception("Cannot call actions on a terminal history")
+        return num_round > 0 and street == 10
 
     def player(self):
         """
@@ -82,47 +56,20 @@ class PreflopHistory():
             return (len(self.history) + 1) % 2
 
     def _game_stage_ended(self):
-        return (
-            (self.history[-1] == "c" and len(self.history) > 3)
-            or self.history[-1] == "f"
-            or self.history[-2:] == ["c", "k"]
-        )
+        last_action = self.history[-1]
+        opp_action = self.history[-2]
+
+        if last_action == "f":
+            return True
+        elif last_action == "c" and len(self.history) > 3:
+            return True
+        elif opp_action == "c" and last_action == "k":
+            return True
+        else:
+            return False
 
     def is_chance(self):
         return self.player() == -1
-
-    def sample_chance_outcome(self):
-        assert self.is_chance()
-
-        if len(self.history) == 0:
-            player_hand = self.round_state.hands[self.active]
-            return "".join(player_hand)
-        elif len(self.history) == 1:
-            opponent_hand = self.round_state.hands[1-self.active]
-            return "".join(opponent_hand)
-        elif self.history[-1] != "/":
-            return "/"
-        else:
-            board_cards = self.round_state.deck[:self.round_state.street]
-            return "".join(board_cards)
-
-    def terminal_utility(self, i: Player) -> int:
-        assert self.is_terminal()  # We can only call the utility for a terminal history
-        assert i in [0, 1]  # Only works for 2 player games for now
-
-    def _get_total_pot_size(self, round_state):
-        # Pot size is the sum of contributions from both players
-        pot_size = round_state.STARTING_STACK - round_state.stacks[0] + \
-                round_state.STARTING_STACK - round_state.stacks[1]
-
-        # Latest bet is the maximum contribution in the current round
-        latest_bet = max(round_state.pips)
-
-        return pot_size, latest_bet
-
-    def __add__(self, action: Action):
-        new_history = PreflopHistory(self.history + [action], self.sample_id)
-        return new_history
 
     def get_infoSet_key(self) -> List[Action]:
         """
